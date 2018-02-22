605f6116d3bf7042f5bbc43edf1de85ccfb42a57
Kevin <you@e>
2018-02-22 16:16:24 +0100

    commit initiale

---
+++ src/main/java/imie/java/tp/tpserver/TpServerApplication.java
@@ -0,0 +1,12 @@
+package imie.java.tp.tpserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpServerApplication.class, args);
+	}
+}
